8110e34b2c1af13b199bc3da0aad99074958283b
Sujata Maity <sujata.maity@ibm.com>
2022-12-19 17:01:19 +0530

    Removed unwanted code

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import CardsWrapper from "./components/CardsWrapper";
 
 const App=()=> {
 
-
   return (
     <div className="App">
      <button onClick={() => window.location.reload()}>Shuffle Cards</button>
