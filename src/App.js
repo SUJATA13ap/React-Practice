b11cd78563b626c0e399222311d6afc741aee68a
Sujata Maity <sujata.maity@ibm.com>
2022-12-19 16:52:57 +0530

    removed unwanted code

--- src/App.js
+++ src/App.js
@@ -4,24 +4,8 @@ import CardsWrapper from "./components/CardsWrapper";
 
 
 const App=()=> {
-  const [message, setMessage] = useState('');
-  const[value, setValue] = useState(""); 
-  var arr = [];
-  
-const fetchCardCount=(event)=>{
-  event.preventDefault();
- var rm=value;
- event.preventDefault();
- console.log("RM",rm);
-  while(arr.length < 8){
-      var r = Math.floor(Math.random() * 13) + 1;
-      if(arr.indexOf(r) === -1) arr.push(r);
-  }
-  setMessage(arr)
-  console.log("Arr",arr);
-}
 
-console.log("Test",message);
+
   return (
     <div className="App">
      <button onClick={() => window.location.reload()}>Shuffle Cards</button>
